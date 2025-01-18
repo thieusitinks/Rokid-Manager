229200fabcf4d102620224ebedb79e1c9d05c957
personxyz <155415297+thieusitinks@users.noreply.github.com>
2025-01-18 17:37:33 +0700

    Update rokidsetup.sh

--- rokidsetup.sh
+++ rokidsetup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 termux-setup-storage && yes | pkg update && yes | pkg upgrade && yes | pkg i python && yes | pkg install python-pip && pkg install python tsu libexpat openssl -y && pip install requests Flask colorama aiohttp psutil crypto pycryptodome loguru prettytable 
-curl -Ls "https://raw.githubusercontent.com/thieusitinks/Rokid-Manager/refs/heads/main/a.py" -o /sdcard/Download/Rokid-Rejoin.py
+curl -Ls "https://raw.githubusercontent.com/thieusitinks/Rokid-Manager/refs/heads/main/Rokid-UgPhone-Free-Tool" -o /sdcard/Download/Rokid-Rejoin.py
